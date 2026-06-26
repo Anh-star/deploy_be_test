5705a69e05213b62ec457a14633714785f373135
Anh-star <anhdh205@gmail.com>
2026-06-27 03:51:06 +0700

    chore: add PayOS config variables placeholder and clean up debug code

--- src/main/java/com/cmcu/itstudy/ItstudyApplication.java
+++ src/main/java/com/cmcu/itstudy/ItstudyApplication.java
@@ -7,18 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItstudyApplication {
 
 	public static void main(String[] args) {
-		String pass = System.getenv("SPRING_DATASOURCE_PASSWORD");
-		String url = System.getenv("SPRING_DATASOURCE_URL");
-		System.out.println("[DEBUG] SPRING_DATASOURCE_URL: " + url);
-		if (pass != null) {
-			System.out.println("[DEBUG] SPRING_DATASOURCE_PASSWORD length: " + pass.length());
-			if (pass.length() >= 3) {
-				System.out.println("[DEBUG] SPRING_DATASOURCE_PASSWORD starts with: " + pass.substring(0, 3));
-				System.out.println("[DEBUG] SPRING_DATASOURCE_PASSWORD ends with: " + pass.substring(pass.length() - 3));
-			}
-		} else {
-			System.out.println("[DEBUG] SPRING_DATASOURCE_PASSWORD is null");
-		}
 		SpringApplication.run(ItstudyApplication.class, args);
 	}
 
