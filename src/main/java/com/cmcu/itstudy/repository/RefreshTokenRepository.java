d2004be5a8ebf49c80cc5c1a7b793038e636366f
Anh-star <anhdh205@gmail.com>
2026-08-19 16:51:25 +0700

    fix(repo): add missing JpaRepository import in RefreshTokenRepository

--- src/main/java/com/cmcu/itstudy/repository/RefreshTokenRepository.java
+++ src/main/java/com/cmcu/itstudy/repository/RefreshTokenRepository.java
@@ -2,6 +2,7 @@ package com.cmcu.itstudy.repository;
 
 import com.cmcu.itstudy.entity.RefreshToken;
 import com.cmcu.itstudy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
